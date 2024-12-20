655c25867d40ce02ebabc6224141b556e58844af
Brett C. Dudo <brett@dudo.io>
2024-12-20 10:36:29 -0800

    get fancy

--- pkg/telemetry/logs.go
+++ pkg/telemetry/logs.go
@@ -2,18 +2,40 @@ package telemetry
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
 // loggingInterceptor logs all incoming gRPC requests
 func LoggingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	start := time.Now()
 
-	slog.Debug("Incoming gRPC request",  "method", info.FullMethod, "request", fmt.Sprintf("%+v", req))
+	if slog.Default().Enabled(ctx, slog.LevelDebug) {
+		if msg, ok := req.(proto.Message); ok {
+			// Use ProtoReflect for detailed introspection
+			reqReflect := msg.ProtoReflect()
+			fields := make(map[string]interface{})
+			reqReflect.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+				fields[string(fd.Name())] = v.Interface()
+				return true
+			})
+			reqDetailsBytes, err := json.Marshal(fields)
+			if err != nil {
+				slog.Error("Error marshaling request fields", "method", info.FullMethod, "error", err)
+			} else {
+				slog.Debug("Incoming gRPC request", "method", info.FullMethod, "request", string(reqDetailsBytes))
+			}
+		} else {
+			// Fallback for non-Protobuf types
+			slog.Debug("Incoming gRPC request", "method", info.FullMethod, "request", fmt.Sprintf("%+v", req))
+		}
+	}
 
 	// Process the request
 	res, err := handler(ctx, req)
