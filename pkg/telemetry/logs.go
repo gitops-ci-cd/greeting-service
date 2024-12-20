f77be26922b74460674b5593bb024ca586563d98
Brett C. Dudo <brett@dudo.io>
2024-12-20 10:16:43 -0800

    again

--- pkg/telemetry/logs.go
+++ pkg/telemetry/logs.go
@@ -7,28 +7,13 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/protobuf/encoding/protojson"
-	"google.golang.org/protobuf/proto"
 )
 
 // loggingInterceptor logs all incoming gRPC requests
 func LoggingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	start := time.Now()
 
-	// Marshal the request to JSON
-	if slog.Default().Enabled(ctx, slog.LevelDebug) {
-		reqProto, ok := req.(proto.Message)
-		if !ok {
-			slog.Warn("Request is not a proto.Message", "type", fmt.Sprintf("%T", req))
-		} else {
-			reqJSON, err := protojson.Marshal(reqProto)
-			if err != nil {
-				slog.Error("Failed to marshal request", "error", err)
-			} else {
-				slog.Debug("Incoming gRPC request", "method", info.FullMethod, "request", string(reqJSON))
-			}
-		}
-	}
+	slog.Debug("Incoming gRPC request",  "method", info.FullMethod, "request", fmt.Sprintf("%+v", req))
 
 	// Process the request
 	res, err := handler(ctx, req)
